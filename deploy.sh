89cadf9f11ab188c7b0bf8874acf1b7bac9dd6c2
HeRo3Sm <hungid6a1@gmail.com>
2023-12-17 17:31:15 +0700

    added deploy file with some tweaks
    the script will work with both debian-based and rehl-based distrobutions
    removed the need to check for git installed in the devices (bruh)

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+PORT=3000
+
+# Check if this machine has python3 or not
+if command -v python3 &> /dev/null
+then 
+    echo "Python3 has been installed"
+else
+    if command -v yum &> /dev/null
+    then
+        echo "Installing python3 using yum"
+        sudo yum install -y python3
+    elif command -v apt &> /dev/null
+    then
+        echo "Installing python3 using apt"
+        sudo apt update && sudo apt install -y python3
+    fi
+fi
+
+# Hosting server 
+if command -v python3 &> /dev/null
+then
+    python3 -m http.server $PORT
+fi
